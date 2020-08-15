8d00d8eaef88fca6f184c339f5edd77013539c8e
BingooDev <BingooDeveloper@gmail.com>
2020-08-15 14:33:36 +0200

    Added tab completion for playernames for /permban

--- DiscordSync/src/main/java/nu/granskogen/spela/DiscordSync/PermbanCommand.java
+++ DiscordSync/src/main/java/nu/granskogen/spela/DiscordSync/PermbanCommand.java
@@ -1,11 +1,19 @@
 package nu.granskogen.spela.DiscordSync;
 
+import java.util.Locale;
 import java.util.UUID;
 
-import net.md_5.bungee.api.CommandSender;
-import net.md_5.bungee.api.plugin.Command;
+import com.google.common.base.Function;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
 
-public class PermbanCommand extends Command {
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.plugin.Command;
+import net.md_5.bungee.api.plugin.TabExecutor;
+
+public class PermbanCommand extends Command implements TabExecutor {
 	Main pl = Main.getInstance();
 
 	public PermbanCommand(String name) {
@@ -44,5 +52,26 @@ public class PermbanCommand extends Command {
 			}
 		});
 	}
+	
+	@Override
+	public Iterable<String> onTabComplete(CommandSender sender, String[] args)
+    {
+        final String lastArg = ( args.length > 0 ) ? args[args.length - 1].toLowerCase( Locale.ROOT ) : "";
+        return Iterables.transform( Iterables.filter( ProxyServer.getInstance().getPlayers(), new Predicate<ProxiedPlayer>()
+        {
+            @Override
+            public boolean apply(ProxiedPlayer player)
+            {
+                return player.getName().toLowerCase( Locale.ROOT ).startsWith( lastArg );
+            }
+        } ), new Function<ProxiedPlayer, String>()
+        {
+            @Override
+            public String apply(ProxiedPlayer player)
+            {
+                return player.getName();
+            }
+        } );
+    }
 
 }
